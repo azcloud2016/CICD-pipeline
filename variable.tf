49e5e8e81faf06b63fe583bda84cb963cd39f611
Sushil Kumar <azcloud2016@gmail.com>
2023-01-16 20:10:05 -0500

    first

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIAX4HH5MUD4AALQHXJ"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "us-east-2"
+  }
